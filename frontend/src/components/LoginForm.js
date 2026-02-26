dd83f023b7bc56268bba3a16d6b8fd414f97e64a
MrManoj124 <manojmanorooban@gmail.com>
2026-02-26 14:31:49 +0530

    create LoginForm function

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -4,3 +4,19 @@ import {User, Lock} from 'lucide-react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
+function LoginForm(){
+    const [formData, setFormdata] = useState({
+        username : "",
+        password : ""
+    });
+
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormdata({
+            ...formData,
+            [e.target.name]:e.target.value
+        });
+    };
+}
