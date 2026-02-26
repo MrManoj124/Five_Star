c24a656ae1b5feaa946c350faaf31314ebc8f568
MrManoj124 <manojmanorooban@gmail.com>
2026-02-26 15:04:22 +0530

    create Function App inside App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,3 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import LoginForm from './components/LoginForm';
 import './App.css';
+
+function App(){
+  return (
+    <Router>
+      <Toaster position="top-right"/>
+      <Routes>
+        <Route path="/" element={<LoginForm/>}></Route>
+      </Routes>
+
+    </Router>
+  );
+}
+
