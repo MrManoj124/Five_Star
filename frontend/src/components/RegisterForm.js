a3e61e2d14c01a334c38504bcfcd7552e9e85cfa
MrManoj124 <manojmanorooban@gmail.com>
2026-03-08 20:38:25 +0530

    import all the files and packages for RegisterForm.js file

---
+++ frontend/src/components/RegisterForm.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { User, Mail, Phone, Lock, Calendar } from 'lucide-react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
