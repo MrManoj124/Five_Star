fac025eb381e0bd4e5ad32ffd7ed3380af2c250a
MrManoj124 <manojmanorooban@gmail.com>
2026-03-08 20:39:10 +0530

    Start to RegisterForm function

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -3,3 +3,5 @@ import { useNavigate, Link } from "react-router-dom";
 import { User, Mail, Phone, Lock, Calendar } from 'lucide-react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
+
+function RegisterForm() {
