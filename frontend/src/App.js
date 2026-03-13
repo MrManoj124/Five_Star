fcbc0cdf321170c50a2aba56f119312ad066bddc
MrManoj124 <manojmanorooban@gmail.com>
2026-03-13 21:05:25 +0530

    import RegisterForm.js into App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import LoginForm from './components/LoginForm';
+import RegisterForm  from "./components/RegisterForm";
 import './App.css';
 
 function App(){
