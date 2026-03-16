4a1dace594ddb33a9b97ce290d9e3ab5fafbbd84
MrManoj124 <manojmanorooban@gmail.com>
2026-03-17 00:17:27 +0530

    Create categories array

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -3,5 +3,5 @@ import { Link } from "react-router-dom";
 import { Package, Wrench, Monitor, Heart, Film, ChevronRight } from 'lucide-react';
 
 function Categories(){
-    
+    const categories = []
 }
