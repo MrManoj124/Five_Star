c40bbe14025ec9bdcdc101367ae1970ceee369f6
MrManoj124 <manojmanorooban@gmail.com>
2026-03-17 00:16:45 +0530

    Start to create Categories function

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,3 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Package, Wrench, Monitor, Heart, Film, ChevronRight } from 'lucide-react';
+
+function Categories(){
+    
+}
